72613e9cbd7476b63057f80511d655946f8d4051
RICH\Sergiusz <serg.dabek@gmail.com>
2018-12-15 15:47:26 +0100

    Threads commit

---
+++ 24_programowanie_funkcyjne/src/main/java/pl/sdacademy/stream/Intro.java
@@ -0,0 +1,11 @@
+package pl.sdacademy.stream;
+
+import pl.sdacademy.user.User;
+
+import java.util.Arrays;
+
+public class Intro {
+        public static void main(String[] args) {
+                Arrays.asList(new User("imie", "nazwisko", 40));
+        }
+}
